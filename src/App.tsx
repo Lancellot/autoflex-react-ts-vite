bec0e425b41e2dd54c6a648716fbe46b968c0b6d
Lancellot <assis.neto@rede.ulbra.br>
2026-02-24 12:26:34 -0300

    feat: implatação do componente navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
+import { ToastContainer } from 'react-toastify/unstyled'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
 
   return (
     <>
+    <ToastContainer />
+    <BrowserRouter>
+      <Navbar />
+      
+      <main className='min-h-[80vh]'>
 
+      </main>
+
+      </BrowserRouter>
     </>
   )
 }

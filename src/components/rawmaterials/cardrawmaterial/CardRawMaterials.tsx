d4b816427226951a5760404c5cac0d16d264acb8
Lancellot <assis.neto@rede.ulbra.br>
2026-02-24 13:22:41 -0300

    feat(rawmaterial): implemetação do card

--- src/components/rawmaterials/cardrawmaterial/CardRawMaterials.tsx
+++ src/components/rawmaterials/cardrawmaterial/CardRawMaterials.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import type RawMaterial from "../../../models/RawMaterials";
 
 interface CardRawMaterialsProps {
@@ -9,10 +10,24 @@ function CardRawMaterials({ rawMaterial }: CardRawMaterialsProps) {
     return (
         <>
             <article className="border flex flex-col rounded-2xl overflow-hidden justify-between">
+
                 <header className="py-2 px-6 bg-blue-500 text-white font-bold text-2xl">{rawMaterial.name}</header>
                 <p className="p-8 text-3xl bg-slate-200 flex-1">{rawMaterial.description}</p>
-                <footer className="flex">
 
+                <footer className="flex">
+                    <Link to={`/editarrawmaterial/${rawMaterial.id}`}
+                        className="w-full text-slate-100 bg-indigo-400 hover:bg-indigo-800
+                                flex items-center justify-center py-2">
+                        <button>Editar</button>
+                    </Link>
+
+                    <Link
+                        to={`/deletarrawmaterial/${rawMaterial.id}`}
+                        className="w-full text-slate-100 bg-red-400 hover:bg-red-700
+                                flex items-center justify-center py-2">
+                        <button>Deletar</button>
+                    </Link>
+                    
                 </footer>
             </article>
         </>
